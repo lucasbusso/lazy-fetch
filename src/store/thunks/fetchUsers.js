d4e833c9fc78b2c6c1e4b4c0100f3c0acd1d2777
Lucas Busso <lucas.busso.cft@jcrew.com>
2023-05-29 21:23:29 -0300

    first thunk

---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,13 @@
+import { createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios"
+
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+    const response = await axios.get('http://localhost:3005/users')
+    return response.data
+})
+//This cAT assign automatically the 3 different states of the requests
+//after the string first parameter like: users/fetch/pending
+//The data will be automatically assigned to the payload property of the
+//fulfilled action type
+
+export { fetchUsers }
