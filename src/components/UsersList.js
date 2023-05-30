ac60c790f990d035339c86b647c4ce71128b895a
Lucas Busso <lucas.busso.cft@jcrew.com>
2023-05-29 23:10:02 -0300

    Skeleton spinner

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,27 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchUsers } from "../store"
+import SkeletonSpinner from "./SkeletonSpinner"
+
 function UsersList() {
+    const dispatch = useDispatch()
+    
+    const {isLoading, data, error} = useSelector((state) => {
+        return state.users
+    })
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    if(isLoading) {
+        return <SkeletonSpinner times={6} className="h-10 w-full"/>
+    }
+    if(error) {
+        return <div>Error fetching data...</div>
+    }
     return(
-        <div>USERS</div>
+        <div>{data.length}</div>
     )
 }
 
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,11 +3,22 @@ import axios from "axios"
 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://localhost:3005/users')
+
+    //DEV ONLY
+    await pause(1300)
+    
     return response.data
 })
 //This cAT assign automatically the 3 different states of the requests
 //after the string first parameter like: users/fetch/pending
 //The data will be automatically assigned to the payload property of the
-//fulfilled action type
+//fulfilled action type 
+
+//DEV ONLY
+const pause = (duration) => {
+    return new Promise ((resolve) => {
+        setTimeout(resolve, duration);
+    })
+}
 
 export { fetchUsers }
