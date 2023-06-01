16a3fc8b0022d04eeaba9bcf5aa0eb36507dce6c
Lucas Busso <lucasxbusso@gmail.com>
2023-05-31 21:28:43 -0300

    Adding component level states to handle the creating and fetching users requests

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,23 +1,37 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchUsers, addUsers, removeUser } from "../store"
 import SkeletonSpinner from "./SkeletonSpinner"
 import Button from './Button'
 
 function UsersList() {
+    const [isLoadingUsers, setIsLoadingUsers] = useState(false)
+    const [loadingUsersError, setLoadingUsersError] = useState(null)
+    const [isCreatingUser, setIsCreatingUser] = useState(false)
+    const [creatingUserError, setCreatingUserError] = useState(null)
+
     const dispatch = useDispatch()
     
-    const {isLoading, data, error} = useSelector((state) => {
-        console.log(state.users.data);
+    const { data } = useSelector((state) => {
         return state.users
     })
 
     useEffect(() => {
-        dispatch(fetchUsers())
-    }, [])
+      setIsLoadingUsers(true);
+      dispatch(fetchUsers())
+        .unwrap() // this function returns a new promise that follows the conventional rules of then and catch. The dispatch promise doesn't because all the results are wrapped inside then function
+        .then(() => console.log("success"))
+        .catch((error) => setLoadingUsersError(error.message))
+        .finally(() => setIsLoadingUsers(false))
+    }, [dispatch, loadingUsersError]);
 
     function handleUserAdd() {
+        setIsCreatingUser(true)
         dispatch(addUsers())
+            .unwrap()
+            .then(() => console.log("user created"))
+            .catch((error) => setCreatingUserError(error))
+            .finally(() => setIsCreatingUser(false))
     }
 
     function handleUserDelete(uderId) {
@@ -25,8 +39,8 @@ function UsersList() {
     }
 
 
-    if(error) {
-        return <div>Error fetching data...</div>
+    if(loadingUsersError) {
+        return <div>Error fetching data: {loadingUsersError}</div>
     }
 
     const renderedUsers = data.map((user) => {
@@ -44,13 +58,15 @@ function UsersList() {
 
     return(
         <div>
-            <div className="flex flex-grow justify-between m3">
+            <div className="flex flex-grow justify-between m-3">
                 <h1 className="m-2 text-xl">Users</h1>
-                <Button onClick={handleUserAdd}>
-                    Add User
-                </Button>
+                { isCreatingUser 
+                    ? 'Creating' 
+                    : <Button onClick={handleUserAdd}>Add User</Button>
+                }
+                { creatingUserError && 'Error creating user' }
             </div>
-            { isLoading 
+            { isLoadingUsers
                 ? ( <SkeletonSpinner times={data.length} className="h-10 w-full"/> )
                 : ( renderedUsers )
             }
