b94fc77a6ab15c98f2ef8555c224e04d314f40c8
Lucas Busso <lucas.busso.cft@jcrew.com>
2023-06-02 18:14:54 -0300

    Adding some kinda dropdown to the user items component in a specific expandable component

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -2,6 +2,8 @@ import Button from './Button'
 import { GoTrashcan } from 'react-icons/go'
 import { removeUser } from '../store'
 import { useThunk } from '../hooks/useThunk'
+import ExpandablePanel from './ExpandablePanel'
+import AlbumsList from './AlbumsList'
 
 function UserListItem({user}) {
     const [doDeleteUser, isDeletingUser, deletingUserError] = useThunk(removeUser)
@@ -10,18 +12,17 @@ function UserListItem({user}) {
         doDeleteUser(user)
     }
 
+    const header = <>
+        <Button className="mr-3" onClick={handleUserDelete} loading={isDeletingUser}>
+            <GoTrashcan />
+        </Button> 
+        {user.name}
+        {deletingUserError && 'Error deleting user'}
+    </>
     return (
-        <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-                <div className='flex flex-row items-center justify-between'>   
-                    <Button className="mr-3" onClick={handleUserDelete} loading={isDeletingUser}>
-                        <GoTrashcan />
-                    </Button> 
-                    {user.name}
-                </div>
-                {deletingUserError && 'Error deleting user'}
-            </div> 
-        </div> 
+        <ExpandablePanel header={header}>
+          <AlbumsList user={user}/>
+        </ExpandablePanel>
     )
 }
 
